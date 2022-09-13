c31daff1c42385f0fb47fa32d079097d6ee772f8
Joan Gavelán <joangavelan@gmail.com>
2022-09-12 20:16:09 -0500

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'eslint-config-prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type']
+  },
+  ignorePatterns: ['vite.config.ts']
+}
