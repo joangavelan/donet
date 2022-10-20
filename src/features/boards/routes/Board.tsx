55ba16db8310260b0d0da89843b61bec129ee1d2
Joan Gavelán <joangavelan@gmail.com>
2022-10-20 14:26:57 -0500

    add helmet to selected board

--- src/features/boards/routes/Board.tsx
+++ src/features/boards/routes/Board.tsx
@@ -1,4 +1,6 @@
 import type { Board as TBoard } from '@/types'
+import { Helmet } from 'react-helmet-async'
+import { toTitleCase } from '@/utils'
 import { Text } from '@chakra-ui/react'
 import { useQueryClient } from 'react-query'
 import { Navigate, useParams } from 'react-router-dom'
@@ -12,5 +14,12 @@ export const Board = () => {
     return <Navigate to='/boards' />
   }
 
-  return <Text textTransform='capitalize'>{board?.id}</Text>
+  return (
+    <>
+      <Helmet>
+        <title>{toTitleCase(board.name)} | Donet</title>
+      </Helmet>
+      <Text textTransform='capitalize'>{board?.id}</Text>
+    </>
+  )
 }
