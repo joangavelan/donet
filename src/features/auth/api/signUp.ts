7e6a2d48ed69a0c7366441fbf5c43973dc6bb722
Joan Gavelán <joangavelan@gmail.com>
2022-10-28 20:54:40 -0500

    refactor

--- src/features/auth/api/signUp.ts
+++ src/features/auth/api/signUp.ts
@@ -1,19 +1,19 @@
 import { supabase } from '@/lib/supabase'
 
-type SignUpProps = {
+type NewUserData = {
   email: string
   password: string
   fullName: string
 }
 
-export const signUp = async ({ fullName, email, password }: SignUpProps) => {
+export const signUp = async ({ fullName, email, password }: NewUserData) => {
   const {
     data: { user },
     error
   } = await supabase.auth.signUp({
     email,
     password,
-    options: { data: { full_name: fullName, signUp: true } }
+    options: { data: { full_name: fullName } }
   })
   if (error) throw error
   return user
