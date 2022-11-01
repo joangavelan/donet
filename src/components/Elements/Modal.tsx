f2ba0fa4c01e49c200fd8cab27aa90afbda50e24
Joan Gavelán <joangavelan@gmail.com>
2022-10-31 21:25:15 -0500

    make top position a prop

--- src/components/Elements/Modal.tsx
+++ src/components/Elements/Modal.tsx
@@ -11,13 +11,20 @@ type ModalProps = {
   children: React.ReactNode
   isOpen: boolean
   onClose: () => void
+  topPosition?: string | number
 }
 
-export const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
+export const Modal = ({
+  title,
+  isOpen,
+  onClose,
+  children,
+  topPosition = '20%'
+}: ModalProps) => {
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose} size='lg'>
       <ModalOverlay />
-      <ModalContent top='20%' pb={4} pt={title ? 0 : 4}>
+      <ModalContent top={topPosition} pb={4} pt={title ? 0 : 4}>
         {title && <ModalHeader>{title}</ModalHeader>}
         <ModalBody>{children}</ModalBody>
       </ModalContent>
