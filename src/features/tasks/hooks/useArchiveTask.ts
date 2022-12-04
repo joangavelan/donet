440e2f54d3a9ce5ca7779d211b3273153c86d3cb
Joan Gavelán <joangavelan@gmail.com>
2022-12-04 17:48:42 -0500

    update indexes of remaining tasks when deleting a task

--- src/features/tasks/hooks/useArchiveTask.ts
+++ src/features/tasks/hooks/useArchiveTask.ts
@@ -1,16 +1,31 @@
 import { useNotification } from '@/hooks'
+import type { Tasks } from '@/types'
 import type { PostgrestError } from '@supabase/supabase-js'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { deleteTask } from '../api'
+import { useUpsertTasks } from './useUpsertTasks'
+
+type Task = Tasks['Row']
 
 export const useArchivetask = () => {
   const showNotification = useNotification()
   const queryClient = useQueryClient()
+  const upsertTasks = useUpsertTasks()
 
   return useMutation(deleteTask, {
     onSuccess: async (archivedTask) => {
-      await queryClient.invalidateQueries(['tasks', archivedTask.template_id])
       showNotification({ type: 'success', message: 'Task archived' })
+
+      // update indexes of remaining tasks
+      queryClient.setQueryData(['tasks', archivedTask.template_id], (tasks) => {
+        const updatedTasks = (tasks as Task[])
+          .filter((task) => task.id !== archivedTask.id)
+          .map((task, index) => ({ ...task, index }))
+
+        upsertTasks.mutate(updatedTasks)
+
+        return updatedTasks
+      })
     },
     onError: (error) => {
       const { message } = error as PostgrestError
