424fd31d5eff8f93c56324f1416ad6de2a2147c4
Joan Gavelán <joangavelan@gmail.com>
2022-12-05 16:21:53 -0500

    change criteria for getting color

--- src/features/tasks/utils/getProgressBarColorScheme.ts
+++ src/features/tasks/utils/getProgressBarColorScheme.ts
@@ -1,10 +1,10 @@
 export const getProgressBarColorScheme = (percentage: number): string => {
   switch (true) {
-    case percentage < 30:
+    case percentage < 40:
       return 'red'
-    case percentage >= 30 && percentage <= 60:
+    case percentage >= 40 && percentage <= 99:
       return 'orange'
-    case percentage > 60:
+    case percentage > 99:
       return 'green'
     default:
       return 'green'
