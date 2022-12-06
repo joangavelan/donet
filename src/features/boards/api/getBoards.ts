13f14a092d1ee6d987bae9513170b208f4b8b67a
Joan Gavelán <joangavelan@gmail.com>
2022-12-06 13:58:16 -0500

    order boards by created at column

--- src/features/boards/api/getBoards.ts
+++ src/features/boards/api/getBoards.ts
@@ -1,7 +1,10 @@
 import { supabase } from '@/lib/supabase'
 
 export const getBoards = async () => {
-  const { data: boards, error } = await supabase.from('boards').select()
+  const { data: boards, error } = await supabase
+    .from('boards')
+    .select()
+    .order('created_at', { ascending: true })
 
   if (error) throw error
 
